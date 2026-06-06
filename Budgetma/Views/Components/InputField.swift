2b00ea5276da75624139f2c27cf1f5235995ec62
Zach Kern <trumpetkern@gmail.com>
2026-06-06 10:32:05 -0400

    improved settings view stuff

--- Budgetma/Views/Components/InputField.swift
+++ Budgetma/Views/Components/InputField.swift
@@ -3,25 +3,34 @@ import SwiftUI
 struct InputField: View {
 	@EnvironmentObject var theme: ThemeManager
 
+	var field: String
 	var placeholder: String
 	@Binding var text: String
 
 	var body: some View {
-		ZStack {
-			if text.isEmpty {
-				Text(placeholder)
-					.foregroundColor(theme.fgColour.opacity(0.8))
-					.listRowBackground(theme.bgColour)
-					.background(theme.bgColour)
-			}
+		HStack {
+			Text(field)
+			.foregroundColor(theme.fgColour)
+			.listRowBackground(theme.bgColour)
+			.background(theme.bgColour)
 
-			TextField("", text: $text)
-				.foregroundColor(theme.fgColour)
-				.tint(theme.fgColour)
-				.listRowBackground(theme.bgColour)
-				.background(theme.bgColour)
+			Spacer()
+
+			ZStack(alignment: .trailing) {
+				if text.isEmpty {
+					Text(placeholder)
+						.foregroundColor(theme.fgColour.opacity(0.8))
+						.listRowBackground(theme.bgColour)
+				}
+
+				TextField("", text: $text)
+					.foregroundColor(theme.fgColour)
+					.tint(theme.fgColour)
+					.listRowBackground(theme.bgColour)
+					.multilineTextAlignment(.trailing)
+			}
+			.scrollContentBackground(.hidden)
+			.background(theme.bgColour)
 		}
-		.scrollContentBackground(.hidden)
-		.background(theme.bgColour)
 	}
 }
--- Budgetma/Views/SettingsView.swift
+++ Budgetma/Views/SettingsView.swift
@@ -1,27 +1,44 @@
 import SwiftUI
 import SwiftData
 
+// main view
 struct SettingsView: View {
 	@EnvironmentObject var theme: ThemeManager
 
 	var body: some View {
-		Form {
-			ColorPicker("Font colour", selection: Binding(
-				get: { theme.fgColour },
-				set: { theme.setForeground($0)}
-			))
-			.listRowBackground(theme.bgColour)
+		ScrollView {
+			VStack(spacing: 5) {
+				ColorPicker("Font colour", selection: Binding(
+					get: { theme.fgColour },
+					set: { theme.setForeground($0)}
+				))
+				.padding()
+				.listRowBackground(theme.bgColour)
 
-			ColorPicker("Background colour", selection: Binding(
-				get: { theme.bgColour },
-				set: { theme.setBackground($0)}
-			))
-			.listRowBackground(theme.bgColour)
+				Divider()
+				.foregroundColor(theme.fgColour)
+				.background(theme.fgColour)
 
-			NavigationLink("Categories") {
-				CategoriesView()
+				ColorPicker("Background colour", selection: Binding(
+					get: { theme.bgColour },
+					set: { theme.setBackground($0)}
+				))
+				.padding()
+				.listRowBackground(theme.bgColour)
+
+				Divider()
+				.foregroundColor(theme.fgColour)
+				.background(theme.fgColour)
+
+				NavigationLink("Categories") {
+					CategoriesView()
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.padding()
+				.listRowBackground(theme.bgColour)
 			}
-			.listRowBackground(theme.bgColour)
+			.scrollContentBackground(.hidden)
+			.background(theme.bgColour)
 		}
 		.scrollContentBackground(.hidden)
 		.background(theme.bgColour)
@@ -29,6 +46,7 @@ struct SettingsView: View {
 
 }
 
+// view for categories
 struct CategoriesView: View {
 	@EnvironmentObject var theme: ThemeManager
 	@Query(
@@ -41,28 +59,53 @@ struct CategoriesView: View {
 	@Environment(\.modelContext)
 	private var context
 	var body: some View {
-		List {
-			ForEach(categories) {category in
-				NavigationLink {
-					CategoryView(category: category)
-				} label: {
-					Text("\(category.emoji) \(category.name)")
-				}
-				.listRowBackground(theme.bgColour)
-				.swipeActions {
-					Button(role: .destructive) {
-						delete(category)
-					} label: {
-						Label("Delete", systemImage: "trash")
+		ScrollView {
+			VStack(spacing: 0) {
+				ForEach(categories) {category in
+					VStack(spacing: 0) {
+						NavigationLink {
+							CategoryView(category: category)
+						} label: {
+							HStack {
+								Text("\(category.emoji) \(category.name)")
+								.foregroundColor(theme.fgColour)
+								.listRowBackground(theme.bgColour)
+
+								Spacer()
+
+								Button(role: .destructive) {
+									delete(category)
+								} label: {
+									Label("", systemImage: "trash")
+								}
+							}
+						}
+						.listRowBackground(theme.bgColour)
+						.padding()
+						.foregroundColor(theme.fgColour)
+						.listRowBackground(theme.bgColour)
+						.frame(maxWidth: .infinity, alignment: .leading)
+
+						Divider()
+						.foregroundColor(theme.fgColour)
+						.background(theme.fgColour)
 					}
+					.foregroundColor(theme.fgColour)
+					.listRowBackground(theme.bgColour)
+					.background(theme.bgColour)
+
 				}
+				NavigationLink {
+					NewCategoryView()
+				} label: {
+					Label("New Category", systemImage: "plus")
+				}
+				.padding()
+				.listRowBackground(theme.bgColour)
+				.frame(maxWidth: .infinity, alignment: .leading)
 			}
-			NavigationLink {
-				NewCategoryView()
-			} label: {
-				Label("New Category", systemImage: "plus")
-			}
-			.listRowBackground(theme.bgColour)
+			.scrollContentBackground(.hidden)
+			.background(theme.bgColour)
 		}
 		.scrollContentBackground(.hidden)
 		.background(theme.bgColour)
@@ -85,6 +128,7 @@ struct CategoriesView: View {
 
 }
 
+// view for an individual category
 struct CategoryView: View {
 	@EnvironmentObject var theme: ThemeManager
 	@Bindable var category: Category
@@ -92,11 +136,22 @@ struct CategoryView: View {
 	private var context
 
 	var body: some View {
-		Form {
-			InputField(placeholder: "Name", text: $category.name)
-			.listRowBackground(theme.bgColour)
-			InputField(placeholder: "Emoji", text: $category.emoji)
-			.listRowBackground(theme.bgColour)
+		ScrollView {
+			VStack {
+				InputField(field: "Name", placeholder: "Something", text: $category.name)
+				.listRowBackground(theme.bgColour)
+				.padding()
+
+				Divider()
+				.foregroundColor(theme.fgColour)
+				.background(theme.fgColour)
+
+				InputField(field: "Emoji", placeholder: "😳", text: $category.emoji)
+				.listRowBackground(theme.bgColour)
+				.padding()
+			}
+			.scrollContentBackground(.hidden)
+			.background(theme.bgColour)
 		}
 		.scrollContentBackground(.hidden)
 		.background(theme.bgColour)
@@ -114,6 +169,7 @@ struct CategoryView: View {
 	}
 }
 
+// new category view
 struct NewCategoryView: View {
 	@EnvironmentObject var theme: ThemeManager
 	@Environment(\.modelContext)
@@ -126,11 +182,22 @@ struct NewCategoryView: View {
 	@State private var emoji = ""
 
 	var body: some View {
-		Form {
-			InputField(placeholder: "Name", text: $name)
-			.listRowBackground(theme.bgColour)
-			InputField(placeholder: "Emoji", text: $emoji)
-			.listRowBackground(theme.bgColour)
+		ScrollView {
+			VStack(spacing: 0) {
+				InputField(field: "Name", placeholder: "Something", text: $name)
+				.listRowBackground(theme.bgColour)
+				.padding()
+
+				Divider()
+				.foregroundColor(theme.fgColour)
+				.background(theme.fgColour)
+
+				InputField(field: "Emoji", placeholder: "😳", text: $emoji)
+				.listRowBackground(theme.bgColour)
+				.padding()
+			}
+			.scrollContentBackground(.hidden)
+			.background(theme.bgColour)
 		}
 		.scrollContentBackground(.hidden)
 		.background(theme.bgColour)
@@ -143,8 +210,8 @@ struct NewCategoryView: View {
 			ToolbarItem(placement: .confirmationAction) {
 				Button("Save") {
 					let category = Category(
-						name: name,
-						emoji: emoji
+						name: name.isEmpty ? "Something" : name,
+						emoji: emoji.isEmpty ? "😳" : emoji
 					)
 
 					let existing = try? context.fetch(
